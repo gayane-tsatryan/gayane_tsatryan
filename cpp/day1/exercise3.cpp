33e3ec4d3ea203cadef96db3b44a23aa2ddaced3
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-03 22:12:47 +0400

    Create exercise3 file for homework N=3.

---
+++ cpp/day1/exercise3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 2023;
+    while (a > 2022 && a <= 2042) {
+        cout << a << " year \n";
+        a++;
+    }
+    return 0;
+}
+
