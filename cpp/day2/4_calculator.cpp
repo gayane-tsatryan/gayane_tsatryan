972148351ccb8b4af178f0b0e80f0d897f89e8fc
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-05 19:38:06 +0400

    Create file for  colculator.

---
+++ cpp/day2/4_calculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream> using namespace std;
+
+int main()
+{
+    char operatr;
+    float number1, number2, result;
+    cout << "Input number 1: ";
+    cin >> number1;
+    cout << "Input operator: ";
+    cin >> operatr;
+    cout << "Input number 2: ";
+    cin >> number2;
+
+    switch (operatr) {
+    case '+': {
+        result = number1 + number2;
+        break;
+    }
+    case '-': {
+        result = number1 - number2;
+        break;
+    }
+    case '*': {
+        result = number1 * number2;
+        break;
+    }
+    case '/': {
+        result = number1 / number2;
+        break;
+    }
+    default: {
+        cout << "Error! Enter operator from (+,-,*,/): \n ";
+    }
+    }
+
+    if (number2 == 0 && operatr == '/') {
+        cout << "Can not divide by 0.\n";
+    }
+    else if (operatr == '+' || operatr == '-' || operatr == '*' || operatr == '/') {
+        cout << number1 << " " << operatr << " " << number2 << " = " << " " << result << "\n";
+    }
+    return 0;
+}
+
