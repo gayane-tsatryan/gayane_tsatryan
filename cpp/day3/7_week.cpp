b7e5c5534862afe44fd869b8d37e931e363e4777
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-07 22:57:13 +0400

    Create program about week day.

---
+++ cpp/day3/7_week.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void weekFunction(int week_number)
+{
+    switch (week_number) {
+    case 1:
+        cout << " Monday \n ";
+        break;
+    case 2:
+        cout << " Tuesday \n ";
+        break;
+    case 3:
+        cout << " Wednesday \n ";
+        break;
+    case 4:
+        cout << " Thursday \n ";
+        break;
+    case 5:
+        cout << " Friday \n ";
+        break;
+    case 6:
+        cout << " Saturday \n ";
+        break;
+    case 7:
+        cout << " Sunday \n ";
+        break;
+    default:
+        cout << " Input number from range (1 - 7):  \n ";
+        break;
+    }
+}
+
+int main()
+{
+    int week_number;
+    cout << " Input  week number(1 - 7) :  ";
+    cin >> week_number;
+    weekFunction(week_number);
+
+    return 0;
+}
