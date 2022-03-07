25d4f813cebbc7dec7b4b38ab2c5fe71bbcd6bfb
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-07 21:57:46 +0400

    Create  programm for game.

---
+++ cpp/day3/1_game.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void gameFunction(int x)
+{
+    int y;
+    cout << "Enter number: \n";
+    do {
+        int y;
+        cin >> y;
+        if (y < x) {
+            cout << "Enter the larg number: \n";
+        }
+        else if (y > x) {
+            cout << "Enter the small number: \n";
+        }
+        else {
+            cout << "You are winner!!! \n";
+            break;
+        }
+
+    } while (y != x);
+}
+
+int main()
+{
+    int a;
+    cout << "Enter number: \n";
+    cin >> a;
+    system("clear");
+
+    gameFunction(a);
+
+    return 0;
+}
