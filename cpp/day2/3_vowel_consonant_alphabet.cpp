5b5edcf42989a9a664d9e28b1c10be52339014e3
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-07 17:07:30 +0400

    Update 3_vowel_consonant_alphabet.cpp

--- cpp/day2/3_vowel_consonant_alphabet.cpp
+++ cpp/day2/3_vowel_consonant_alphabet.cpp
@@ -6,29 +6,29 @@ int main()
     char letter;
     cout << "Input alphabet : ";
     cin >> letter;
-
-    switch (letter) {
-    case 'a':
-    case 'e':
-    case 'i':
-    case 'o':
-    case 'u':
-    case 'y':
-    case 'A':
-    case 'E':
-    case 'I':
-    case 'O':
-    case 'Y':
-    case 'U': {
-        cout << " '" << letter << "'  is  vowel. \n";
-        break;
+    if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')) {
+        switch (letter) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'y':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'Y':
+        case 'U':
+            cout << " '" << letter << "'  is  vowel. \n";
+            break;
+        default:
+            cout << " '" << letter << "'  is  consonant. \n";
+            break;
+        }
     }
-    default: {
-        cout << " '" << letter <<  "'  is  consonant. \n";
-        break;
+    else {
+        cout << "Error!  Enter letter: \n";
     }
-    }
-
     return 0;
 }
-
