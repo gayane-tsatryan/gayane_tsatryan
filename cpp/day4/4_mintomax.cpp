530a6078f70690e2c4264fd61280d1570a96194d
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-09 22:32:18 +0400

    Create program for array min to max.

---
+++ cpp/day4/4_mintomax.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void minToMax(int);
+int main()
+{
+    int length;
+    cout << "Enter the number  for array's length:  ";
+    cin >> length;
+    minToMax(length);
+    return 0;
+}
+void minToMax(int length)
+{
+    int array[length];
+    int temp = 0;
+    for (int i = 0; i < length; i++) {
+        array[i] = rand() % 300;
+    }
+    for (int i = 0; i < length; i++) {
+        cout << array[i] << ", ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < (length - 1); i++) {
+        for (int j = 0; j < (length - i - 1); j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < length; i++) {
+        cout << array[i] << ", ";
+    }
+
+    cout << endl;
+}
+
