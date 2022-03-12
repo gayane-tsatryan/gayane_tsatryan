e696b1e7c49afb52ac7e38fc485735ec73044e29
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-13 01:38:06 +0400

    Create program to check row   if elements is pair.

---
+++ cpp/day6/4_pair_rownumber.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void matrix();
+int main()
+{
+    matrix();
+    return 0;
+}
+void matrix()
+{
+    const int row = 5;
+    const int column = 7;
+    int array[row][column];
+    int temp = 0;
+    int temp1 = 0;
+    string number = "";
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            array[i][j] = rand()%70;
+            if (array[i][j] % 2 == 0) {
+                temp++;
+            }
+        }
+
+        if (temp == column) {
+            temp1++;
+            cout << i << endl;
+            break;
+        }
+        temp = 0;
+    }
+
+    if (temp1 == 0) {
+        cout << "No!" << endl;
+    }
+}
+
