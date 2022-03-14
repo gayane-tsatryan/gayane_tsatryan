834eba9d0b70a64d5b602c0a29b613ca6f2d3faf
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-15 01:48:57 +0300

    Create program to find max and min value of array.

---
+++ cpp/day7/5_minAndMaxValueOfArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void maxMinValueOfArray(int length) {
+  int max = 0;
+  int min = 0;
+  int * parray = new int[length];
+  for (int i = 0; i < length; i++) {
+    *(parray + i) = rand() % 10 + 1;
+    cout << * (parray + i);
+  }
+  cout << endl;
+  min = * (parray);
+  max = * (parray);
+  for (int i = 0; i < length; i++) {
+    if (min > * (parray + i)) {
+      min = * (parray + i);
+    } else {
+      max = * (parray + i);
+    }
+
+  }
+  cout << "min = " << min << endl << "max = " << max;
+
+}
+int main() {
+  int length;
+  cout << "Input the length of array: ";
+  cin >> length;
+  maxMinValueOfArray(length);
+  return 0;
+}
