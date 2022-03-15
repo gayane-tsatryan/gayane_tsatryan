041b8facafe3ce2ac2beece794554651ada1beeb
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-15 23:04:30 +0300

    Create .cpp file for  my_atoi function.

---
+++ cpp/day8/atoi.cpp
@@ -0,0 +1,19 @@
+#include "atoi.h"
+#include <iostream>
+
+int my_atoi(char* a)
+{
+    int result = 0;
+    for (int i = 0; a[i] != '\0'; i++) {
+        if (a[i] >= 48 && a[i] <= 57) {
+            result = result * 10 + a[i] - '0';
+        }
+        else {
+            result = 0;
+        }
+    }
+
+    return result;
+}
+
+
