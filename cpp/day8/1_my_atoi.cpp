bb136b0e80e4c48eb54d0d8efb18857a8314633c
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-16 19:00:14 +0300

    Update 1_my_atoi.cpp

--- cpp/day8/1_my_atoi.cpp
+++ cpp/day8/1_my_atoi.cpp
@@ -1,17 +1,16 @@
 #include "atoi.h"
 #include <iostream>
 using namespace std;
-int my_atoi(char* a)
-{
-    int result = 0;
-    for (int i = a[0] == '-' ? 1 : 0; a[i] != '\0'; i++) {
-        if (a[i] >= 48 && a[i] <= 57) {
-            result = result * 10 + a[i] - '0';
-        }
-        else {
-            result = 0;
-        }
-    }
 
-    return a[0] == '-' ? -result : result;
+int main(int argc, char * argv[])
+{
+    int sum1 = 0, sum2 = 0;
+    for(int *ptr = argv; ptr != (argv + argc); ptr++)
+    {
+        sum1 += my_atoi (*ptr);
+        sum2 += atoi (*ptr);
+    }
+    cout << "my_atoi - " << sum1 << endl; 
+    cout << "atoi - " << sum2 << endl; 
+    return 0;
 }
