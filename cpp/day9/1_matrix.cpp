79412de03d09fe3e47d4c42e82b77e7edf77b57b
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-17 02:19:31 +0300

    Update 1_matrix.cpp

--- cpp/day9/1_matrix.cpp
+++ cpp/day9/1_matrix.cpp
@@ -2,24 +2,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-        int height, width;
-        cout << "Enter height: ";
-        cin >> height;
-        cout << "Enter width: ";
-        cin >> width;
-        int**  matrix = new int*[height];
-        for (int** ptr = matrix; ptr < matrix + height; ptr++) {
-                *ptr = new int[width];
-                for (int* ptr1 = *ptr; ptr1 < *ptr + width; ptr1++) {
-                        *ptr1 = rand() % 9 + 1 ; 
-                }
+int main()
+{
+    int height, width;
+    cout << "Enter height: ";
+    cin >> height;
+    cout << "Enter width: ";
+    cin >> width;
+    int** matrix = new int*[height];
+    for (int** ptr = matrix; ptr < matrix + height; ptr++) {
+        *ptr = new int[width];
+        for (int* ptr1 = *ptr; ptr1 < *ptr + width; ptr1++) {
+            *ptr1 = rand() % 9 + 1;
         }
-        print_matrix(matrix, width, height);
-        cout << endl;
-        sort_matrix(matrix, width, height);
-        cout << endl;
-        print_matrix(matrix, width, height);
-        cout << endl;
-        return 0;
+    }
+    print_matrix(matrix, width, height);
+    cout << endl;
+    sort_matrix(matrix, width, height);
+    cout << endl;
+    print_matrix(matrix, width, height);
+    cout << endl;
+    sortMinToMax(matrix, width, height);
+    print_matrix(matrix, width, height);
+    cout << endl;
+    return 0;
 }
