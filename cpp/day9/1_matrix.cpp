3568310fa914a105a8f55b26cc0e93c2e2b05197
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-16 15:59:53 +0300

    Create program for print and sorting matrix.

---
+++ cpp/day9/1_matrix.cpp
@@ -0,0 +1,22 @@
+#include "matrix.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+        int height = 5;
+        int width = 5;
+        int**  matrix = new int*[height];
+        for (int** ptr = matrix; ptr < matrix + height; ptr++) {
+                *ptr = new int[width];
+                for (int* ptr1 = *ptr; ptr1 < *ptr + width; ptr1++) {
+                        *ptr1 = rand() % 9 + 1 ; 
+                }
+        }
+        print_matrix(matrix, width, height);
+        cout << endl;
+        sort_matrix(matrix, width, height);
+        cout << endl;
+        print_matrix(matrix, width, height);
+        cout << endl;
+        return 0;
+}
