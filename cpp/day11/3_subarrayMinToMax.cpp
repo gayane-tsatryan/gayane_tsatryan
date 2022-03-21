7ef64f9a9813481a4492ddf305fec9db1c98017c
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-21 20:28:35 +0400

    Create program for find subarray min to max.

---
+++ cpp/day11/3_subarrayMinToMax.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subArr(int array[], int length);
+
+int main()
+{
+    int length;
+    cout << "Enter array length: ";
+    cin >> length;
+    int array[length];
+    for (int i = 0; i < length; i++) {
+        array[i] = rand() % 100 + 1;
+    }
+    subArr(array, length);
+    cout << endl;
+    return 0;
+}
+
+void subArr(int array[], int n)
+{
+    int max = 1, temp = 1, index = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (array[i] > array[i - 1])
+            temp++;
+        else {
+            if (max < temp) {
+                max = temp;
+
+                index = i - max;
+            }
+
+            temp = 1;
+        }
+    }
+
+    if (max < temp) {
+        max = temp;
+        index = n - max;
+    }
+
+    for (int i = index; i < max + index; i++)
+        cout << array[i] << " ";
+}
