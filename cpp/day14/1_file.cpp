d2c7c48c3999f967714f20319b58e1ec60518f6c
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-03-29 14:45:27 +0400

    Update 1_file.cpp

--- cpp/day14/1_file.cpp
+++ cpp/day14/1_file.cpp
@@ -39,7 +39,7 @@ int main()
         cin.ignore();
         toStr(datainput.dataDate, &dd, &mm, &yy);
 
-        if ((mm == 2 && isLeap(yy) && dd > 0 && dd <= 28) || (mm == 2 && !isLeap(yy) && dd > 0 && dd <= 29) || ((mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12) && dd > 0 && dd <= 31) || (mm == 4 || mm == 6 || mm == 9 || mm == 11) && dd > 0 && dd <= 30) {
+        if ((mm == 2 && isLeap(yy) && dd > 0 && dd <= 29) || (mm == 2 && !isLeap(yy) && dd > 0 && dd <= 28) || ((mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12) && dd > 0 && dd <= 31) || (mm == 4 || mm == 6 || mm == 9 || mm == 11) && dd > 0 && dd <= 30) {
             getline(cin, datainput.Note);
             string dayname = "";
             int wday1 = weekDay(yy, mm, dd);
