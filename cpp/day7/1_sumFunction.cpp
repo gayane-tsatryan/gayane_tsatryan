807f83770e0bdbdf707e500f77f8bd12c5c6aee4
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-06 22:17:49 +0400

    Update 1_sumFunction.cpp

--- cpp/day7/1_sumFunction.cpp
+++ cpp/day7/1_sumFunction.cpp
@@ -23,15 +23,15 @@ void sum5(int * a, int * b, int & sum) {
   cout << * a << " + " << * b << " = " << sum << endl;
 }
 int main() {
-  int number1, number2, n = 0;
+  int number1, number2, sum = 0;
   cout << "Enter the first number: ";
   cin >> number1;
   cout << "Enter the second number: ";
   cin >> number2;
-  sum1( & number1, & number2, & n);
-  sum2(number1, number2, n);
-  sum3(number1, number2, n);
-  sum4(number1, & number2, n);
-  sum5( & number1, & number2, n);
+  sum1( & number1, & number2, & sum);
+  sum2(number1, number2, sum);
+  sum3(number1, number2, sum);
+  sum4(number1, & number2, sum);
+  sum5( & number1, & number2, sum);
   return 0;
 }
