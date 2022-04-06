6681da1532106e107dda1a4ac08970fa69ad9e78
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-06 21:24:36 +0400

    Create program to print leap year.

---
+++ cpp/day1/3_leap_year.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year = 2024;
+    int i = 0;
+    while (i < 20) {
+
+        cout << year << " year \n";
+        year += 4;
+        i++;
+    }
+    return 0;
+}
