9a570d3241543ae6822b0ad99dfe08b5b7bf8d6f
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-06 21:41:40 +0400

    Create program for star pictures.

---
+++ cpp/day2/5_star.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int quantity, i = 0;
+    cout << "Enter count: \n";
+    cin >> quantity;
+    const int count = quantity;
+    i = count;
+
+    while (i > 0) {
+        int j = 0;
+        while (j < i) {
+            cout << " * ";
+            j++;
+        }
+
+        cout << endl;
+        j = 0;
+        i--;
+    }
+
+    while (i < count) {
+        int j = 0;
+        while (j <= i) {
+            cout << " * ";
+            j++;
+        }
+        j = 0;
+        cout << " " << endl;
+        i++;
+    }
+
+    i = 0;
+    while (i < count) {
+        int j = 0;
+        while (j <= count) {
+            if (j < count - i) {
+                cout << " ";
+            }
+            else {
+                cout << "*";
+            }
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    i = 0;
+
+    while (i < count) {
+        int j = 0;
+        while (j <= count + i) {
+            if (j < count - i) {
+                cout << " ";
+            }
+            else {
+                cout << "*";
+            }
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    i = 0;
+    while (i < count) {
+        int j = 0;
+        while (j <= count) {
+            if (j < count - i) {
+                cout << " ";
+            }
+            else {
+                cout << " *";
+            }
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
