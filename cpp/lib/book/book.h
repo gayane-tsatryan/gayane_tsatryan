d6529e0e6639d7872ca652d6883a344328c99f3a
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-06 22:39:03 +0400

    Create book.h

---
+++ cpp/lib/book/book.h
@@ -0,0 +1,9 @@
+int size(string );
+bool goToRegion();
+char to_upper(char );
+string toUpWord(string );
+bool stob(string);
+int size();
+bool isBig(string, string);
+void toStr(string a, int*, int*, int*);
+void addFile(string , string , int );
