5d7e6463b85dc284b5b6e3df5b33312e38b62995
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-11 23:19:43 +0400

    Create homosapience.cpp file for body h...h file.

---
+++ oop/homosapience.cpp
@@ -0,0 +1,19 @@
+#include "human.h";
+#include "homosapience.h";
+
+HomoSapience::Party()
+{
+    cout << "Party";
+}
+HomoSapience::Work()
+{
+    cout << "Work";
+}
+HomoSapience::Dance()
+{
+    cout << "Dance";
+}
+HomoSapience::GoToGym()
+{
+    cout << "GoToGym";
+}
