386db89ca0fd6dada433f27347612b9b7e581f58
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-13 00:22:21 +0400

    Create class polynomial.

---
+++ oop/day2/polynomial.h
@@ -0,0 +1,11 @@
+class polynomial {
+    int a[' '], b[' '], c[' '], i, j, m, n, x;
+
+public:
+    void get_date();
+    void Add();
+    void SubSract();
+    void Multiplay();
+    void MultiplayByNumber();
+    void display();
+};
