874f86ec0449c673f52e09ad745bb08577138c36
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-15 02:38:52 +0400

    Update programmer.h

--- oop/programmer.h
+++ oop/programmer.h
@@ -1,7 +1,11 @@
 #include "human.h";
 #include "homosapience.h"
 
-class Programmer : public Human, public Homosapience {
-    private  string _compSkills;
-    public void HardWork();
+class Programmer: public Homosapience
+{
+	private: string _compSkills;
+
+	public: void HardWork();
+	string getCompSkills() const;
+	void setCompSkills(string compskills);
 };
