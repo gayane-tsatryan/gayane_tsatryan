1b93f967e166dd01e8a829c685794ce975d724c7
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-14 17:58:27 +0400

    Update polynomial.h

--- oop/day2/polynomial.h
+++ oop/day2/polynomial.h
@@ -4,7 +4,7 @@ public:
     int _degree = 0;
 
     int GetData();
-    int Display(int *coeff, int degree);
+    int Display(int *coeff, int _degree);
     void Add(polynomial P1, polynomial P2);
     void Substract(polynomial P1, polynomial P2);
     void Multiply(polynomial P1, polynomial P2);
