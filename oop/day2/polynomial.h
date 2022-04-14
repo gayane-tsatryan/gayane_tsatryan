d5aacc426a93169e91f1bbcbf372f554c37a7548
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-14 23:10:56 +0400

    Update polynomial.h

--- oop/day2/polynomial.h
+++ oop/day2/polynomial.h
@@ -9,4 +9,17 @@ public:
     void Substract(polynomial P1, polynomial P2);
     void Multiplay(polynomial P1, polynomial P2);
     void MultByNumber(int a, polynomial P);
+    
+    polynomial Plus(const polynomial&) const;
+    polynomial operator+(const polynomial&) const;
+    polynomial &operator+=(const polynomial&);
+    polynomial Minus(const polynomial&) const;
+    polynomial operator-(const polynomial&) const;
+    polynomial &operator-=(const polynomial&);
+    polynomial Mult(const polynomial&) const;
+    polynomial operator*(const polynomial&) const;
+    polynomial &operator*=(const polynomial&);
+    bool operator==(const polynomial&) const;
+    std::ostream &operator<<(std::ostream&, const polynomial&);
+    std::istream &operator>>(std::istream&, polynomial&);
 };
