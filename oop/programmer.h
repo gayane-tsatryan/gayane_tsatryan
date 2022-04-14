97c745f0162d745b310f472915a7e9678f882d9d
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-15 02:57:49 +0400

    Update programmer.h

--- oop/programmer.h
+++ oop/programmer.h
@@ -3,11 +3,18 @@
 
 class Programmer: public Homosapience
 {
-	private: string _compSkills;
-
-	public: void HardWork();
-	string getCompSkills() const;
-	void setCompSkills(string compskills);
-	virtual void Work() override;
-	virtual void hobby() override;
+	private: 
+		string _compSkills = "";
+        	unsigned long _salary = 0;
+	public: 
+		void HardWork();
+		string getCompSkills() const;
+		void setCompSkills(string compskills);
+		unsigned long getSalary() const;
+		void setSalary(unsigned long salary);
+		virtual void Work() override;
+		virtual void hobby() override;
+		Programmer(const HomoSapience& homoSapience, string compSkills, unsigned int salary);
+                Programmer(const Programmer&);
+                ~Programmer();
 };
