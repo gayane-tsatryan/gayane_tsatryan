761909fe5e2524801725547391f78a3f7779b83f
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-15 03:01:34 +0400

    Update programmer.cpp

--- oop/programmer.cpp
+++ oop/programmer.cpp
@@ -1,48 +1,74 @@
 #include "human.h";
 #include "homosapience.h";
 
-Programmer :: HardWork()
+Programmer::Programmer(const HomoSapience &homoSapience, string compSkills, unsigned int salary): HomoSapience(homoSapience)
 {
-   cout << "HardWork";
+	_compSkills = compSkills;
+	_salary = salary;
+}
+
+string programmer::getCompSkills() const
+{
+	return _compSkills;
+}
+void programmer::setCompSkills(string compskills)
+{
+	_compSkills = compskills;
+}
+unsigned long programmer::getSalary() const
+{
+	return _salary;
+}
+void programmer::setSalary(unsigned long salary)
+{
+	_salary = salary;
+}
+Programmer::HardWork()
+{
+	int _getSalary = getSalary();
+	setSalary(++_getSalary);
 }
 Programmer::Party()
 {
-    eat(sweat);
-    drink(juice);
+	eat(sweat);
+	drink(juice);
 }
-Programmer::Work() 
+Programmer::Work()
 {
-        int _health = getHealthLevel();
-		setHealthLevel(--_health);
-		setBadHealth(getMentalHealth() + getHealthLevel());
-		if (getBadHealth() == 0)
-		{
-			cout << "Die";
-		}
-        if (getHungry()<=120)
-        {
-        eat(fruits);
-        }
-            
+	int _health = getHealthLevel();
+	setHealthLevel(--_health);
+	setBadHealth(getMentalHealth() + getHealthLevel());
+	if (getBadHealth() == 0)
+	{
+		cout << "Die";
+	}
+	if (getHungry() <= 120)
+	{
+		eat(fruits);
+	}
 }
 Programmer::Dance()
 {
-    int _health = getHealthLevel();
-    int _mental = getMentalHealth();
-    setHealtLevel(++_health);
-    setMentalHealth(++_mental);
-    eat(vegetable);
-    drink(water);
-    
+	int _health = getHealthLevel();
+	int _mental = getMentalHealth();
+	setHealtLevel(++_health);
+	setMentalHealth(++_mental);
+	eat(vegetable);
+	drink(water);
+
 }
 Programmer::GoToGym()
 {
-    
-    int _health = getHealthLevel();
-    int _mental = getMentalHealth();
-    setHealtLevel(++_health);
-    setMentalHealth(++_mental);
-    eat(vegetable);
-    drink(water);
-}
 
+	int _health = getHealthLevel();
+	int _mental = getMentalHealth();
+	setHealtLevel(++_health);
+	setMentalHealth(++_mental);
+	eat(vegetable);
+	drink(water);
+}
+Programmer::Hobby()
+{
+	cout << "Play guitar!";
+
+}
