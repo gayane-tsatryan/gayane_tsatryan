06f9f9564b88c0e7eaf3b5f2a75e8dc7a12278a4
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-15 02:36:28 +0400

    Update homosapience.cpp

--- oop/homosapience.cpp
+++ oop/homosapience.cpp
@@ -1,19 +1,111 @@
 #include "human.h";
 #include "homosapience.h";
 
-HomoSapience::Party()
+HomoSapience::HomoSapience(string address, string name, string surname, string fatherName, string mothername, Religion religion, Nationality nationality)
 {
-    cout << "Party";
+	_address = address;
+	_name = name;
+	_surname = surame;
+	_fatherName = fatherName;
+	_mothername = motherName;
+	_religion = religion;
+	_nationality = nationality;
+
 }
-HomoSapience::Work()
+HomoSapience::HomoSapience(string address, string name, string surname, string mothername, Religion religion, Nationality nationality)
 {
-    cout << "Work";
+	_address = address;
+	_name = name;
+	_surname = surame;
+	_mothername = motherName;
+	_religion = religion;
+	_nationality = nationality;
+
 }
-HomoSapience::Dance()
+HomoSapience::HomoSapience(string address, string name, string surname, string fatherName, Religion religion, Nationality nationality)
 {
-    cout << "Dance";
+	_address = address;
+	_name = name;
+	_surname = surame;
+	_fatherName = fatherName;
+	_religion = religion;
+	_nationality = nationality;
+
 }
-HomoSapience::GoToGym()
+HomoSapience::HomoSapience(string address, string name, string surname, Religion religion, Nationality nationality)
 {
-    cout << "GoToGym";
+	_address = address;
+	_name = name;
+	_surname = surame;
+	_religion = religion;
+	_nationality = nationality;
+
+}
+
+string HomoSapience::getAddress() const
+{
+	return _address;
+}
+void setAddress(string address)
+{
+	_address = address;
+}
+
+string HomoSapience::getName() const
+{
+	return _name;
+}
+void HomoSapience::setName(string name)
+{
+	_name = name;
+}
+
+string HomoSapience::getSurname() const
+{
+	return _surname;
+}
+void HomoSapience::setSurname(string surname)
+{
+	_surname = surname;
+}
+
+string HomoSapience::getFatherName() const
+{
+	return _fatherName;
+}
+void HomoSapience::setFatherName(string fatherName)
+{
+	_fatherName = fatherName;
+}
+
+string HomoSapience::getMotherName() const
+{
+	return _motherName;
+}
+void HomoSapience::setMotherName(string motherName)
+{
+	_motherName = motherName;
+}
+Nationality HomoSapience::getNationality() const
+{
+	return _nationality;
+}
+void HomoSapience::setNationality(Nationality nationality)
+{
+	if (nationality == arm || nationality == rus || nationality == eng)
+	{
+		_nationality = nationality;
+	}
+}
+
+Religion HomoSapience::getReligion() const
+{
+	return _religion;
+}
+void HomoSapience::setReligion(Religion religion)
+{
+	if (religion == Christianity || religion == caballa || religion == islam)
+	{
+		_religion = religion;
+	}
 }
