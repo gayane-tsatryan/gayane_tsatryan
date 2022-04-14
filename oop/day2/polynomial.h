6a324052b66fbc5fe94d707ea7e0268b2e5a311d
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-14 17:54:34 +0400

    Update polynomial.h

--- oop/day2/polynomial.h
+++ oop/day2/polynomial.h
@@ -1,11 +1,11 @@
 class polynomial {
-  public int a[' '], b[' '], c[' '], i, j, m, n, x;
-
 public:
-    void get_date();
-    void Add();
-    void SubSract();
-    void Multiplay();
-    void MultiplayByNumber();
-    void display();
+    int *coeff, degree;
+
+    int GetData();
+    int Display(int *coeff, int degree);
+    void Add(polynomial P1, polynomial P2);
+    void Substract(polynomial P1, polynomial P2);
+    void Multiply(polynomial P1, polynomial P2);
+    void MultByNumber(int a);
 };
