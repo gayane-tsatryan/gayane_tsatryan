30cf04adadaf6e320f4622e0048266b79c7aefc2
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-15 01:52:25 +0400

    Update homosapience.h

--- oop/homosapience.h
+++ oop/homosapience.h
@@ -1,22 +1,52 @@
-#include "human.h"
-enum Nationality { arm, rus, eng };
-enum Religion { Christianity,
-    caballa,
-    islam };
-class HomoSapience : public Human {
-private:
-    string _address;
-    string _name;
-    string _surname;
-    string _fatherName;
-    string _motherName;
-    Nationality _nationality;
-    Religion _religion;
 
-public:
-    void Party();
-    void Work();
-    void Dance();
-    void GoToGym();
-    HomoSapience();
+#include "human.h"
+
+enum Nationality
+{
+	arm, rus, eng
+};
+enum Religion
+{
+	Christianity,
+	caballa,
+	islam
+};
+class HomoSapience: public Human
+{
+	private: string _address;
+	string _name;
+	string _surname;
+	string _fatherName;
+	string _motherName;
+	Nationality _nationality;
+	Religion _religion;
+
+	public: string getAddress() const;
+	void setAddress(string address);
+
+	string getName() const;
+	void setName(string name);
+
+	string getSurname() const;
+	void setSurname(string surname);
+
+	string getFatherName() const;
+	void setFatherName(string fatherName);
+
+	string getMotherName() const;
+	void setMotherName(string motherName);
+
+	Nationality getNationality() const;
+	void setNationality(Nationality nationality);
+
+	Religion getReligion() const;
+	void setReligion(Religion religion);
+
+	void Party();
+	virtual void Work();
+	virtual void hobby();
+	void Dance();
+	void GoToGym();
+	public: HomoSapience(string address, string name, string surname, string fatherName, string mothername, Religion religion, Nationality nationality);
+	HomoSapience();~HomoSapience();
 };
