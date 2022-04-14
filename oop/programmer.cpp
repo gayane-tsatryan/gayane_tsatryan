407d4513b84081d4e88b6f6854c1f37413baabf8
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-15 02:19:32 +0400

    Update programmer.cpp

--- oop/programmer.cpp
+++ oop/programmer.cpp
@@ -5,3 +5,44 @@ Programmer :: HardWork()
 {
    cout << "HardWork";
 }
+Programmer::Party()
+{
+    eat(sweat);
+    drink(juice);
+}
+Programmer::Work() 
+{
+        int _health = getHealthLevel();
+		setHealthLevel(--_health);
+		setBadHealth(getMentalHealth() + getHealthLevel());
+		if (getBadHealth() == 0)
+		{
+			cout << "Die";
+		}
+        if (getHungry()<=120)
+        {
+        eat(fruits);
+        }
+            
+}
+Programmer::Dance()
+{
+    int _health = getHealthLevel();
+    int _mental = getMentalHealth();
+    setHealtLevel(++_health);
+    setMentalHealth(++_mental);
+    eat(vegetable);
+    drink(water);
+    
+}
+Programmer::GoToGym()
+{
+    
+    int _health = getHealthLevel();
+    int _mental = getMentalHealth();
+    setHealtLevel(++_health);
+    setMentalHealth(++_mental);
+    eat(vegetable);
+    drink(water);
+}
+
