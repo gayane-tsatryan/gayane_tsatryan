bcf367ca1e110fc1e7a8496bbdfadf549f41bea3
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-18 00:27:35 +0400

    Update polynomial.cpp

--- oop/day2/polynomial.cpp
+++ oop/day2/polynomial.cpp
@@ -1,9 +1,19 @@
 #include "polynomial.h"
-#include <vector>
 #include <string>
-#include <sstream>
 #include <iostream>
 
+polynomial::polynomial(int* coef, int degree)
+{
+    _degree = degree;
+    coeff = new int[degree];
+    for (int i = 0; i < degree; i++) {
+        coeff[i] = coef[i];
+    }
+}
+polynomial::~polynomial()
+{
+    delete[] coeff;
+}
 int polynomial::GetData()
 {
     int i = 0;
@@ -93,7 +103,7 @@ void polynomial::Substract(polynomial P1, polynomial P2)
 void polynomial::Multiplay(polynomial P1, polynomial P2)
 {
     int i = 0;
-    int j = 0; 
+    int j = 0;
     int _max = 0;
     _max = P1._degree + P2._degree;
     int* mul = new int[_max + 1];
@@ -106,14 +116,94 @@ void polynomial::Multiplay(polynomial P1, polynomial P2)
 void polynomial::MultByNumber(int _number, polynomial P1)
 {
     int i = 0;
-    int j = 0; 
+    int j = 0;
     int _max = 0;
     _max = P1._degree;
     int* mul = new int[_max + 1];
 
-    for (i = P1._degree; i >= 0; i--){
-            mul[i] += P1.coeff[i] * _number;
+    for (i = P1._degree; i >= 0; i--) {
+        mul[i] += P1.coeff[i] * _number;
     }
     Display(mul, _max);
 }
 }
+polynomial polynomial::operator+(polynomial P2)
+{
+    int _max = 0;
+    int i = 0;
+    _max = (this->_degree > P2._degree) ? this->_degree : P2._degree;
+    int* res = new int[_max + 1];
+
+    for (i = 0; i <= _max; i++) {
+        res[i] = this->coeff[i] + P2.coeff[i];
+    }
+
+    return polynomial(res, _max);
+}
+polynomial polynomial::operator+=(polynomial P2)
+{
+    *this = *this + P2;
+    return *this;
+}
+polynomial polynomial::operator-(polynomial P2)
+{
+    int _max = 0;
+    int i = 0;
+    _max = (this->_degree > P2._degree) ? this->_degree : P2._degree;
+    int* res = new int[_max + 1];
+
+    for (i = 0; i <= _max; i++) {
+        res[i] = this->coeff[i] - P2.coeff[i];
+    }
+
+    return polynomial(res, _max);
+}
+
+polynomial polynomial::operator-=(polynomial P2)
+{
+    *this = *this - P2;
+    return *this;
+}
+polynomial polynomial::operator*(polynomial P2)
+{
+    int i = 0;
+    int j = 0;
+    int _max = 0;
+    _max = this->_degree + P2._degree;
+    int* res = new int[_max + 1];
+
+    for (i = this->_degree; i >= 0; i--)
+        for (j = P2._degree; j >= 0; j--)
+            res[i + j] += this->coeff[i] * P2.coeff[j];
+
+    return polynomial(res, _max);
+}
+
+polynomial polynomial::operator*=(polynomial P2)
+{
+    *this = *this * P2;
+    return *this;
+}
+
+std::istream& operator>>(std::istream& in, polynomial& poly)
+{
+    std::string input;
+    std::getline(in >> std::ws, input);
+    poly = input;
+    return in;
+}
+std::ostream& operator<<(std::ostream& out, const polynomial& poly)
+{
+    for (int i = poly._degree; i >= 0; i--) {
+        out << poly.coeff[i];
+        if(poly.coeff[i] == 0)
+        {
+           continue;
+        }
+        if (i != 0) {
+            out << "x^" << i << "+";
+        }
+    }
+    return out;
+}
+
