590a3cd150dc1b263d02092f7b92ec096a5f0d24
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-17 23:21:00 +0400

    Update polynomial.h

--- oop/day2/polynomial.h
+++ oop/day2/polynomial.h
@@ -1,4 +1,47 @@
-class polynomial {
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class polynomial
+{
+public:
+    char var = 'x^';
+    const _degree;
+    polynomial();
+    polynomial(const poly&, const char variable);
+    polynomial(const poly&);
+    polynomial(const vector<int>&);
+    polynomial &operator=(const poly&);
+    void add(const polynomial&);
+    polynomial plus(const polynomial&) const;
+    polynomial operator+(const polynomial&) const;
+    polynomial &operator+=(const polynomial&);
+    void subtract(const polynomial&);
+    polynomial minus(const polynomial&) const;
+    polynomial operator-(const polynomial&) const;
+    polynomial &operator-=(const polynomial&);
+    void multiply(const polynomial&);
+    polynomial times(const polynomial&) const;
+    polynomial operator*(const polynomial&) const;
+    polynomial &operator*=(const polynomial&);
+    bool operator==(const polynomial&) const;
+    void print(ostream&) const;
+    void print(ostream&, const char variable) const;
+
+private:
+    vector<int> coeff;
+};
+
+polynomial add(const polynomial&, const polynomial&);
+polynomial subtract(const polynomial&, const polynomial&);
+polynomial multiply(const polynomial&, const polynomial&);
+
+std::ostream &operator<<(ostream&, const polynomial&);
+std::istream &operator>>(istream&, polynomial&);
+
+
+/*class polynomial {
 public:
     int *coeff;
     int _degree = 0;
@@ -24,3 +67,4 @@ public:
     std::ostream &operator<<(std::ostream&, const polynomial&);
     std::istream &operator>>(std::istream&, polynomial&);
 };
+*/
