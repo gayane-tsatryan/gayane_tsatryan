03cc66d83ee34655a341330a76d5aef3c21abe4e
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-19 06:48:21 +0400

    Create date.h file.

---
+++ oop/day1/date.h
@@ -0,0 +1,24 @@
+#ifndef DATE_H
+#define DATE_H
+class Date {
+  
+  private:
+    int _day;
+    int _month;
+    int _year;
+
+  public:
+    Date(int day, int month, int year);
+  
+    int getDay() const;
+    void setDay(int day);
+  
+    int getMonth() const;
+    void setMonth(int month);
+  
+    int getYear() const;
+    void setYear(int year);
+    
+    string getDate() const;
+};
+#endif
