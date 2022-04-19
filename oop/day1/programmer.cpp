8b80ab7dc5fc0236a9591773d45667e7c5263054
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-19 07:41:55 +0400

    Create programmer.cpp

---
+++ oop/day1/programmer.cpp
@@ -0,0 +1,77 @@
+#include "employee.h"
+
+Programmer::Programmer(const employee &employee, string compSkills, unsigned int salary, unsigned int bonus, ProgrammerType programmerType, ProgrammerLevel programmerLevel, string company): employee(employee)
+{
+	_compSkills = compSkills;
+	_salary = salary;
+  _bonus = bouns;
+  _programmerType = programmerType;
+  _programmerLevel = programmerLevel;
+  _company = company;
+}
+
+string programmer::getCompSkills() const
+{
+	return _compSkills;
+}
+void programmer::setCompSkills(string compskills)
+{
+	_compSkills = compskills;
+}
+unsigned long programmer::getSalary() const
+{
+	return _salary;
+}
+void programmer::setSalary(unsigned long salary)
+{
+	_salary = salary;
+}
+Programmer::HardWork()
+{
+	int _getSalary = getSalary();
+	setSalary(++_getSalary);
+}
+Programmer::Party()
+{
+	eat(sweat);
+	drink(juice);
+}
+Programmer::Work()
+{
+	int _health = getHealthLevel();
+	setHealthLevel(--_health);
+	setBadHealth(getMentalHealth() + getHealthLevel());
+	if (getBadHealth() == 0)
+	{
+		cout << "Die";
+	}
+	if (getHungry() <= 120)
+	{
+		eat(fruits);
+	}
+}
+Programmer::Dance()
+{
+	int _health = getHealthLevel();
+	int _mental = getMentalHealth();
+	setHealtLevel(++_health);
+	setMentalHealth(++_mental);
+	eat(vegetable);
+	drink(water);
+
+}
+Programmer::GoToGym()
+{
+
+	int _health = getHealthLevel();
+	int _mental = getMentalHealth();
+	setHealtLevel(++_health);
+	setMentalHealth(++_mental);
+	eat(vegetable);
+	drink(water);
+}
+Programmer::Hobby()
+{
+	cout << "Play guitar!";
+
+}
