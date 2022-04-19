d493d3107f0f6a3c8ab7c0ac991477f46a433657
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-19 07:04:04 +0400

    Create employee.h

---
+++ oop/day1/employee.h
@@ -0,0 +1,29 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include "homosapience.h"
+
+
+class employee : public homosapience {
+private:
+    string _idNumber = "";
+    string _department = "";
+    string _position = "";
+    int _yearsWorked = 0;
+
+public:
+    employee(const homosapience& homosapience, string idNum, string depart, string pos, int yrsWrkd);
+    employee(const homosapience& homosapience, string idNum);
+    employee();
+    ~employee();
+
+    void setIdnumber(string idNum);
+    void setDepartment(string depart);
+    void setPos(string pos);
+    void setYEARS(int yrsWrkd);
+    string const getID();
+    string const getDPT();
+    string const getPOS();
+    int const getYEARS();
+};
+#endif
