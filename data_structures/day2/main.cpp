5634d8930c2c600e84aad32a30200c9a8154970a
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-25 23:45:50 +0400

    Update main.cpp

--- data_structures/day2/main.cpp
+++ data_structures/day2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "LinkedList.h"
 #include "LinkedList.cpp"
+#include "stack.h"
 using namespace std;
 
 int main()
@@ -8,11 +9,16 @@ int main()
     LinkedList<int> list;
 
     for (int i = 0; i < 10; i++) {
-        int _value = rand() % 20 + 1;
+        int _value = rand() % 10 + 1;
         list.Add(_value);
     }
-    cout << list;
+    list.Add(12);
     list.Add(22);
-    list.removeAt(1);
-    cout << list;
+    list.removeAt(4);
+    cout << list << endl;
+    Stack<int> stack;
+    stack.push(12);
+    stack.push(23);
+    cout << stack.peek() << endl;
+    cout << stack.pop() << endl;
 }
