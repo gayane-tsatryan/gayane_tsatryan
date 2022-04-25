57f83fd0f9bd2b30ae84e0af3ba45ca2d9a178cb
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-04-25 22:58:41 +0400

    Create LinkedList.h file.

---
+++ data_structures/day2/LinkedList.h
@@ -0,0 +1,47 @@
+#ifndef LINKED_LIST_h
+#define LINKED_LIST_h
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Node {
+public:
+    T _value;
+    Node<T>* _next;
+    Node<T>(T value, Node<T>* next = nullptr)
+    {
+        _value = value;
+        _next = next;
+    }
+};
+
+template <class T>
+class LinkedList {
+    Node<T>* _head;
+    Node<T>* _tail;
+    int _count;
+
+public:
+    void Add(T);
+    void AddAt(T, int);
+    void removeAt(int);
+    void clear();
+    T getElement(const int) const;
+    int GetCount() const;
+
+    template <class Type>
+    friend ostream& operator<<(ostream& out, const LinkedList<Type>&);
+
+    LinkedList()
+    {
+        _count = 0;
+        _head = nullptr;
+        _tail = nullptr;
+    }
+    ~LinkedList()
+    {
+        clear();
+    }
+};
+#endif
