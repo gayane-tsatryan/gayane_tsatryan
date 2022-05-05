dbd7ab044c8098762efd066b3c27f851af04bbeb
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-05-05 07:13:38 +0400

    Create  remove method.

--- data_structures/day7/aliasMain.cpp
+++ data_structures/day7/aliasMain.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 class BinaryTreeNode {
 public:
+    BinaryTreeNode* Left;
+    BinaryTreeNode* Right;
+    int Value;
     BinaryTreeNode(int value)
     {
         Value = value;
     }
-    BinaryTreeNode* Left;
-    BinaryTreeNode* Right;
-    int Value;
 };
 
 class BinaryTree {
@@ -45,6 +45,7 @@ public:
         _count++;
     }
 
+private:
     void AddTo(BinaryTreeNode* node, int value)
     {
 
@@ -69,14 +70,6 @@ public:
         }
     }
 
-    bool Contains(int value)
-    {
-
-        BinaryTreeNode* parent = NULL;
-        return (FindWithParent(value, parent) != NULL);
-    }
-
-private:
     BinaryTreeNode* FindWithParent(int value, BinaryTreeNode* parent)
     {
 
@@ -103,9 +96,115 @@ private:
         return current;
     }
 
+public:
+    bool Contains(int value)
+    {
+
+        BinaryTreeNode* parent = NULL;
+        return (FindWithParent(value, parent) != NULL);
+    }
+
     int Count()
     {
+
         return _count;
+    }
+    bool Remove(int value)
+    {
+        BinaryTreeNode* current;
+        BinaryTreeNode* parent = NULL;
+
+        current = FindWithParent(value, parent);
+
+        if (current == NULL) {
+            return false;
+        }
+
+        _count--;
+
+        if (current->Right == NULL) {
+
+            if (parent == NULL) {
+                _head = current->Left;
+            }
+
+            else {
+                int result = CompareTo(parent->Value, current->Value);
+
+                if (result > 0) {
+
+                    parent->Left = current->Left;
+                }
+
+                else if (result < 0) {
+
+                    parent->Right = current->Left;
+                }
+            }
+        }
+
+        else if (current->Right->Left == NULL) {
+            current->Right->Left = current->Left;
+
+            if (parent == NULL) {
+                _head = current->Right;
+            }
+
+            else {
+                int result = CompareTo(parent->Value, current->Value);
+                if (result > 0) {
+                    parent->Left = current->Right;
+                }
+                else if (result < 0) {
+                    parent->Right = current->Right;
+                }
+            }
+        }
+
+        else {
+
+            BinaryTreeNode* leftmost = current->Right->Left;
+            BinaryTreeNode* leftmostParent = current->Right;
+
+            while (leftmost->Left != NULL)
+
+            {
+                leftmostParent = leftmost;
+                leftmost = leftmost->Left;
+            }
+
+            leftmostParent->Left = leftmost->Right;
+            leftmost->Left = current->Left;
+            leftmost->Right = current->Right;
+
+            if (parent == NULL) {
+                _head = leftmost;
+            }
+
+            else
+
+            {
+                int result = CompareTo(parent->Value, current->Value);
+
+                if (result > 0) {
+                    parent->Left = leftmost;
+                }
+
+                else if (result < 0)
+
+                {
+                    parent->Right = leftmost;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void Clear()
+    {
+        _head = NULL;
+        _count = 0;
     }
 };
 
@@ -116,6 +215,9 @@ int main()
     instance->Add(8);
     instance->Add(5);
     instance->Add(9);
+    instance->Add(10);
+    instance->Add(7);
+    instance->Remove(8);
     cout << instance->_head->Right->Value << endl;
     cout << instance->_head->Left->Value << endl;
     cout << instance->Contains(18);
