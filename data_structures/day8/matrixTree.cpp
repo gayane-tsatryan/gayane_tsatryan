3d204424aeda92d5199e28d7cecf42fa00e31ad2
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-05-05 18:23:39 +0400

    Create tree use matrix.

---
+++ data_structures/day8/matrixTree.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class BinaryTreeNode {
+public:
+    BinaryTreeNode* Left;
+    BinaryTreeNode* Right;
+    int Value;
+    BinaryTreeNode(int value)
+    {
+        Left = NULL;
+        Right = NULL;
+        Value = value;
+    }
+};
+
+class BinaryTree {
+private:
+    int _count;
+
+public:
+    BinaryTreeNode* _head;
+
+    void print(BinaryTree* root)
+    {
+        if (root == NULL)
+            return;
+
+        queue<BinaryTreeNode*> q;
+
+        q.push(root->_head);
+
+        while (!q.empty()) {
+
+            BinaryTreeNode* tree = q.front();
+            cout << tree->Value << " ";
+            q.pop();
+
+            if (tree->Left != NULL)
+                q.push(tree->Left);
+
+            if (tree->Right != NULL)
+                q.push(tree->Right);
+        }
+    }
+    int CompareTo(int value1, int value2)
+    {
+        if (value1 > value2) {
+            return 1;
+        }
+        else if (value1 < value2) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    bool Contains(int value)
+    {
+
+        BinaryTreeNode* parent = NULL;
+        return (FindWithParent(value, parent) != NULL);
+    }
+
+    BinaryTreeNode* FindWithParent(int value, BinaryTreeNode* parent)
+    {
+
+        BinaryTreeNode* current = _head;
+        parent = NULL;
+
+        while (current != NULL) {
+            int result = CompareTo(current->Value, value);
+            if (result > 0) {
+
+                parent = current;
+                current = current->Left;
+            }
+            else if (result < 0) {
+
+                parent = current;
+                current = current->Right;
+            }
+            else {
+
+                break;
+            }
+        }
+        return current;
+    }
+
+    void Add(int value)
+    {
+
+        if (_head == NULL) {
+            _head = new BinaryTreeNode(value);
+        }
+
+        else {
+            AddTo(_head, value);
+        }
+        _count++;
+    }
+
+private:
+    void AddTo(BinaryTreeNode* node, int value)
+    {
+
+        if (CompareTo(node->Value, value) > 0) {
+
+            if (node->Left == NULL) {
+                node->Left = new BinaryTreeNode(value);
+            }
+            else {
+                AddTo(node->Left, value);
+            }
+        }
+        else {
+
+            if (node->Right == NULL) {
+                node->Right = new BinaryTreeNode(value);
+            }
+            else {
+
+                AddTo(node->Right, value);
+            }
+        }
+    }
+};
+
+void Input(int** arr, int size)
+{
+    for (int i = 0; i < size; i++) {
+        arr[i] = new int[size];
+        for (int j = 0; j < size; j++) {
+            cin >> arr[i][j];
+        }
+    }
+}
+
+bool isBinaryTree(int** arr, int size, vector<int>& matrixIndexes)
+{
+    Input(arr, size);
+
+    bool row = true;
+    int temp2 = 0;
+
+    for (int i = 0; i < size; i++) {
+        int _temp = 0;
+        for (int j = 0; j < size; j++) {
+            if (arr[i][j] == 1) {
+                ++_temp;
+                if (_temp > 2) {
+                    row = false;
+                }
+            }
+            if (arr[i][j] == arr[j][i] && arr[j][i] == 1) {
+                row = false;
+            }
+
+            if (arr[i][j] == 1) {
+                matrixIndexes.push_back(j);
+            }
+        }
+
+        int count = 0;
+        for (int k = 0; k < size; k++) {
+            if (arr[k][i] == 0) {
+                ++count;
+            }
+            if (count == size) {
+                ++temp2;
+            }
+        }
+    }
+    if (temp2 != 1) {
+        row = false;
+    }
+    if (row == false) {
+        cout << "It isn't a binary tree! " << endl;
+    }
+    else {
+        cout << "It is a binary tree!" << endl;
+    }
+
+    return row;
+}
+int getRoot(int** arr, int size, vector<int>& matrixIndexes)
+{
+    int index = -1;
+    bool isbin = isBinaryTree(arr, size, matrixIndexes);
+    if (isbin) {
+        for (int i = 0; i < size; i++) {
+            int count = 0;
+            for (int j = 0; j < size; j++) {
+                if (arr[j][i] == 0) {
+                    ++count;
+                }
+            }
+            if (count == size) {
+                index = i;
+            }
+        }
+    }
+    return index;
+}
+
+int main()
+{
+    int size = 4;
+    int** arr = new int*[size];
+
+    vector<int> matrixIndexes;
+    int _length = getRoot(arr, size, matrixIndexes);
+    BinaryTree* tree = new BinaryTree();
+
+    if (_length != -1) {
+        tree->Add(_length);
+        for (int i = 0; i < matrixIndexes.size(); i++) {
+            tree->Add(matrixIndexes[i]);
+        }
+    }
+    tree->print(tree);
+
+    return 0;
+}
