a9afb832dee95fae46c788c202bcbf778fcbee7a
gayane-tsatryan <100441121+gayane-tsatryan@users.noreply.github.com>
2022-10-29 00:21:44 +0400

    Create file for Homework_1.  About data types.

---
+++ Java/Homework_1.java
@@ -0,0 +1,58 @@
+public class Homework_1 {
+    public static void main(String[] args) {
+       
+        byte a = -128;
+        byte b = +127;
+
+        short c = -32768;
+        short d = +32767;
+
+        int e = -2147483648;
+        int f = +2147483647;
+
+
+        long g = -9223372036854775808l;
+        long h = +9223372036854775807L;
+
+        float i = -0.123456789f;  
+        float j = +1.123456789F; 
+
+        double k = -0.123456789d;
+        double l = +1.123456789D;
+        double k1 = -0.123456789f;
+        double l1 = +1.123456789F;
+        double l2 = +1.123456789;
+
+        boolean m = true;
+        boolean n = false;
+
+     
+        char o = 'a';
+        char p = 'A';
+        char q = 0x0048;    
+        char r = '\u0063';
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(k1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(r);
+     
+    }
+}
